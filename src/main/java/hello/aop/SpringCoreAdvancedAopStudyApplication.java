dd4bc62aa12cec8b910c7d6a8a579b3c9aeedbcb
장정우 <jungwoo@lotte.net>
2024-12-16 15:21:43 +0900

    first commit

---
+++ src/main/java/hello/aop/SpringCoreAdvancedAopStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAdvancedAopStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreAdvancedAopStudyApplication.class, args);
+    }
+
+}
